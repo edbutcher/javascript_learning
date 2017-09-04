623568c08532c8ebf5706be198a83cc3cfdc2440
edbutcher <edbutcher200@gmail.com>
2017-09-04 23:20:20 +0300

    js basic

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-console.log("Starting app");
+console.log("Starting app.js");
 
 const fs = require("fs"); // like import
 const os = require("os");
+const _ = require("lodash");
 
-var user = os.userInfo();
+const notes = require("./notes");
 
+
+
+
+// var fiteredArray = _.uniq(["edbutcher", 5, "edbutcher", 1, 2, 3, 4, 5, 6])
+// console.log(fiteredArray);
+
+// console.log(_.isString(true));
+// console.log(_.isString("Alex"));
+// console.log(_.isString(14));
+
+// console.log("Results:", notes.add(5 , 6));
+// var user = os.userInfo();
 // is deprecated.
 // fs.appendFile("greetings.txt", "Hello world");
 // full version
@@ -14,4 +27,4 @@ var user = os.userInfo();
 //   }
 // });
 // es6
-fs.appendFileSync("greetings.txt", `Hello ${user.username}!`);
+// fs.appendFileSync("greetings.txt", `Hello ${user.username}! You are ${notes.age}.`);
