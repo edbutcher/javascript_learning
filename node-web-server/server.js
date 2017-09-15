acfc68f9356e7cfedd5a2d9ab6ec277dad008c45
edbutcher <edbutcher200@gmail.com>
2017-09-15 23:16:48 +0300

    Node.js web server

---
+++ node-web-server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/about', (request, respose) => {
+  respose.render('about.hbs', {
+    pageTitle: 'About page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/', (request, respose) => {
+  respose.render('home.hbs', {
+    pageTitle: 'Home page',
+    welcomeMsg: 'Welcome to my node.js website!',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/bad', (request, respose) => {
+  respose.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
--- weather-app/app_promise.js
+++ weather-app/app_promise.js
@@ -23,7 +23,8 @@ axios.get(geocodeUrl).then((response) => {
   }
   var lat = response.data.results[0].geometry.location.lat;
   var lng = response.data.results[0].geometry.location.lng;
-  var weatherUrl = `https://api.darksky.net/forecast/f84e395ad316a0d039797f41e5516f04/${lat},${lng}`;
+  //////Change key for darksky.net
+  var weatherUrl = `https://api.darksky.net/forecast/f84e395abd316a0d039797f41e5516f04/${lat},${lng}`;
   console.log(response.data.results[0].formatted_address);
   return axios.get(weatherUrl);
 }).then((response) => {
